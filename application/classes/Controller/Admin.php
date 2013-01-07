8dfbd4527b51156980e54a715097c29a64bdaf30
Kamil Badura <kbadura@future-processing.com>
2013-01-07 14:38:18 +0100

    Dodanie controlera admin

---
+++ application/classes/Controller/Admin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kbadura
+ * Date: 1/7/13
+ * Time: 2:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Controller_Admin extends Controller_Layout
+{
+
+
+    public function __construct(Request $request, Response $response)
+    {
+        $this->navbar="layout/navbar-admin";
+        $this->title="Admin";
+
+        parent::__construct($request,$response);
+    }
+
+    public function action_index()
+    {
+
+    }
+}
--- application/views/layout/index.php
+++ application/views/layout/index.php
@@ -8,24 +8,22 @@
 <html lang="pl">
 <head>
 <title>
-<?php if ($title): ?>
-    <?php include $title; ?>
-<?php endif ?>
+    <?php echo $title; ?>
 </title>
 </head>
 <body>
-<header>
-<?php if ($navbar): ?>
-    <?php include $navbar; ?>
-<?php endif ?>
-</header>
-<div id="content">
-    <?php if ($content): ?>
-        <?php include $content; ?>
-    <?php endif ?>
-</div>
+    <header>
+        <?php if ($navbar): ?>
+            <?php include $navbar; ?>
+        <?php endif ?>
+    </header>
+    <div id="content">
+        <?php if ($content): ?>
+            <?php include $content; ?>
+        <?php endif ?>
+    </div>
 
-<?php //include Kohana::find_file('views', 'profiler/stats', 'php') ?>
-<?php //include Kohana::find_file('views', 'common/bottom', 'php') ?>
-</body>
+    <?php //include Kohana::find_file('views', 'profiler/stats', 'php') ?>
+    <?php //include Kohana::find_file('views', 'common/bottom', 'php') ?>
+    </body>
 </html>
