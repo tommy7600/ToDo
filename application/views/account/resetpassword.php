e4dba98c5119afc6fc78dc61210c44930dedd10f
Tomasz Lorenc <tlorenc@future-processing.com>
2013-01-08 14:57:18 +0100

    Add resetpassword view prototype

--- application/views/account/resetpassword.php
+++ application/views/account/resetpassword.php
@@ -1,1 +1,14 @@
-Reset password
+<h2>Login</h2>
+<div class="login_form">
+    <form class="formClass" method="post" action="">
+        <fieldset>
+            <label class="pw_cont" for="password"><span>Password</span></label>
+            <input id="password" name="password" class="validate[required] text-input" placeholder="password" type="password" tabindex="1">
+        </fieldset>
+        <fieldset>
+            <label class="pw_cont" for="password_conf"><span>Password</span></label>
+            <input id="password_conf" name="password_conf" class="validate[required] text-input" placeholder="confirm password" type="password" tabindex="2">
+        </fieldset>
+        <fieldset class="login_submit"><button type="submit" class="btn">CHANGE PASSWORD</button></fieldset>
+    </form>
+</div>
