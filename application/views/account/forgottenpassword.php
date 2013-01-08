0e87f1138e217bad632ebadce89de5e4fc1b89cd
Tomasz Lorenc <tlorenc@future-processing.com>
2013-01-08 14:16:19 +0100

    Layout: Fix get -> post

--- application/views/account/forgottenpassword.php
+++ application/views/account/forgottenpassword.php
@@ -1,9 +1,9 @@
 <h2>Forgot Password</h2>
 <div class="login_form">
-    <form class="formClass" action="">
+    <form class="formClass" method="post" action="">
         <fieldset>
-            <label class="email_cont" for="email_f"><span>Email</span></label>
-            <input id="email_f" name="email_f" placeholder="email" class="validate[custom[email]] text-input"  type="text" tabindex="1" accesskey="u">
+            <label class="email_cont" for="email"><span>Email</span></label>
+            <input id="email" name="email" placeholder="email" class="validate[custom[email]] text-input"  type="text" tabindex="1" accesskey="u">
         </fieldset>
         <fieldset class="login_submit"><button type="submit" class="btn">RECOVER PASSWORD</button></fieldset>
     </form>
--- application/views/account/registration.php
+++ application/views/account/registration.php
@@ -1,17 +1,17 @@
 <h2>Sign up</h2>
 <div class="login_form">
-    <form class="formClass" action="">
+    <form class="formClass" method="post" action="">
         <fieldset>
-            <label class="email_cont" for="email_s"><span>Email</span></label>
-            <input id="email_s" name="email_s" class="validate[custom[email]] text-input" placeholder="email" type="text" tabindex="1" accesskey="e">
+            <label class="email_cont" for="email"><span>Email</span></label>
+            <input id="email" name="email" class="validate[custom[email]] text-input" placeholder="email" type="text" tabindex="1" accesskey="e">
         </fieldset>
         <fieldset>
-            <label for="username_s"><span>Username</span></label>
-            <input id="username_s" name="username_s" class="validate[required] text-input" placeholder="username" type="text" tabindex="2" accesskey="u">
+            <label for="username"><span>Username</span></label>
+            <input id="username" name="username" class="validate[required] text-input" placeholder="username" type="text" tabindex="2" accesskey="u">
         </fieldset>
         <fieldset>
-            <label class="pw_cont" for="password_s"><span>Password</span></label>
-            <input id="password_s" name="password_s" class="validate[required] text-input" placeholder="password" type="password" tabindex="3" accesskey="p">
+            <label class="pw_cont" for="password"><span>Password</span></label>
+            <input id="password" name="password" class="validate[required] text-input" placeholder="password" type="password" tabindex="3" accesskey="p">
         </fieldset>
         <fieldset class="login_submit"><button type="submit" class="btn">SIGN UP</button></fieldset>
         <fieldset class="login_social">
