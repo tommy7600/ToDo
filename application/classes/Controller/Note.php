ca7d32fc02605ff2095fc5f500a743fa023e0a66
Kamil Dworak <k.dworak@future-processing.com>
2013-01-09 11:47:17 +0100

    merge

--- application/classes/Controller/Note.php
+++ application/classes/Controller/Note.php
@@ -1,14 +1,10 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-/**
- * Created by JetBrains PhpStorm.
- * User: kbadura
- * Date: 1/7/13
- * Time: 11:26 AM
- * To change this template use File | Settings | File Templates.
- */
+
 class Controller_Note extends Controller_Layout
 {
+
     protected $title = "ToDo - Note";
+
 
     public function before()
     {
